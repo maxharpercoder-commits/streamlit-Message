113a971f51ad13d2568a26919342fa11d67c294b
Max <max.harper.coder@gmail.com>
2026-03-23 17:25:45 +0000

    Fix page1 function definition and navigation setup

--- Main.py
+++ Main.py
@@ -3,8 +3,11 @@ import streamlit as st
 def page2():
     st.title("Second page")
 
+def page1():
+    st.title("Second page")
+
 pg = st.navigation([
-    st.Page("page1.py", title="First page", icon="🔥"),
+    st.Page(page1, title="First page", icon="🔥"),
     st.Page(page2, title="Second page", icon=":material/favorite:"),
 ])
 pg.run()
