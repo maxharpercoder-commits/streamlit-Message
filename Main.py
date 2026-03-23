7a5621a4577146d3653025bb8c19239b9d69a96c
Max <max.harper.coder@gmail.com>
2026-03-23 17:37:12 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ def page2():
 
 def page1():
     st.title("Messaged is locked")
-    global password=st.text_input("enter password")
+    global password = st.text_input("enter password")
     if password=="bob":
         st.Page(page2, title="Second page", icon=":material/favorite:"),
 
