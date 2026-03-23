e373199bde483629438c507e317be11a0be5e848
Max <max.harper.coder@gmail.com>
2026-03-23 17:47:45 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 # streamlit_app.py
 import streamlit as st
-
+def page2():
+    st.title("Second page")
 def page1():
     st.title("Login")
     password = st.text_input("Enter password", type="password")
     if password == "bob":
         st.switch_page(page2())
 
-def page2():
-    st.title("Second page")
+
 
 pg = st.navigation([st.Page(page1, title="Login"), st.Page(page2, title="Second page")])
 pg.run()
