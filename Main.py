ef0f64f19123897999bcce7d60035282a3ede975
Max <max.harper.coder@gmail.com>
2026-03-23 17:21:10 +0000

    Refactor navigation and password handling in Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,4 @@
 import streamlit as st
-pg = st.navigation([
-st.Page(page1, title="First page", icon="🔥"),
-st.Page(page2, title="Second page", icon=":material/favorite:"),
-    ])
-password = ""
 def page2():
     st.text('Messages is unlocked')
 def page1():
@@ -11,7 +6,12 @@ def page1():
     password = st.text_input('Enter the password to unlock Messages', type='password')
 if password == "RUNEATM11@#":
     pg.run()
-
 else:
     pass
+pg = st.navigation([
+st.Page(page1, title="First page", icon="🔥"),
+st.Page(page2, title="Second page", icon=":material/favorite:"),
+    ])
+password = ""
+
 
